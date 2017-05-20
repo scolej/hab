bc2bc0850f8d12fced7b69179eb247830ea8d8a9
sjc <s.cole.j@gmail.com>
2017-05-20 22:05:35 +1000

    Fix massive noob mistake.

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -2,11 +2,14 @@ import qualified CharDecHealthTest
 import qualified CharIncExpTest
 import qualified GameTest
 import Test.HUnit
+import System.Exit
 
 tests :: Test
 tests = TestList [CharDecHealthTest.tests, CharIncExpTest.tests, GameTest.tests]
 
 main :: IO ()
 main = do
-  runTestTT tests
-  return ()
+  Counts cases tried errors failures <- runTestTT tests
+  if errors > 0 || failures > 0
+    then exitFailure
+    else exitSuccess
